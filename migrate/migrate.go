9fa7c65575491ffb6ca02d2d3481064dd071fa92
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2025-03-08 15:42:52 +0900

    fix: dbのportを5432に修正

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 	"go-rest-api/db"
 	"go-rest-api/model"
+	"go-rest-api/util"
+	"os"
+
+	"gorm.io/gorm"
 )
 
 func main() {
-	dbConn := db.NewDB()
+	var dbConn *gorm.DB
+	if os.Getenv("GO_ENV") == "test" {
+		dbConn = util.NewTestDB()
+	} else {
+		dbConn = db.NewDB()
+	}
 	defer fmt.Println("Successfully migrated")
 	defer db.CloseDB(dbConn)
 	dbConn.AutoMigrate(&model.User{}, &model.Task{})
